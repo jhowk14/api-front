a425df21a06149b1146bfc15eff0431e1ecab463
jhowk14 <jonathanperozi16@gmail.com>
2023-12-15 15:46:38 -0300

    15/12

--- src/services/redis.ts
+++ src/services/redis.ts
@@ -1,8 +1,15 @@
 import Redis from 'ioredis';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 // Configurar a conexão com o servidor Redis local
-export const redis = new Redis({
-  host: '192.168.0.188', // ou nome de host
-  port: 6379,               // Porta padrão do Redis
-});
+const { REDIS_URL } = process.env;
+
+// Verifique se a URL do Redis está presente no .env
+if (!REDIS_URL) {
+  throw new Error('A variável de ambiente REDIS_URL não está definida no arquivo .env');
+}
+
+// Crie uma instância do cliente Redis com base na URL
+export const redis = new Redis(REDIS_URL);
 
